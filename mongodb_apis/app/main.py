5c98a3bac892b730fdf798e56a912ff65e55be06
nathanbangwa243 <nathanbangwa7@gmail.com>
2025-02-17 12:31:41 +0200

    [REF] mongodb_apis: update main.py to read env

--- mongodb_apis/app/main.py
+++ mongodb_apis/app/main.py
@@ -3,6 +3,19 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("GEMINI_MODEL_NAME")
+
+print("AWS_ACCESS_KEY_ID : ", AWS_ACCESS_KEY_ID)
+print("GEMINI_MODEL_NAME : ", AWS_SECRET_ACCESS_KEY)
+
+env_str = f"AWS_ACCESS_KEY_ID : {AWS_ACCESS_KEY_ID} - AWS_SECRET_ACCESS_KEY : {AWS_SECRET_ACCESS_KEY}"
+
 # if __name__ == "app.main":
 #     # for tests purpose
 #     from .routers import *
@@ -12,7 +25,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_main():
-    return {"message": "Welcome to your new MongoDB API"}
+    return {"message": "Welcome to your new MongoDB API", "env": env_str}
 
 # ADD ROUTERS
 
