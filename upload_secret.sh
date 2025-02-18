85c81f6de54889e84e10ec0c0f9286013512d4a5
nathanbangwa243 <nathanbangwa7@gmail.com>
2025-02-18 10:24:12 +0200

    [ADD] .github: update actions

--- upload_secret.sh
+++ upload_secret.sh
@@ -1,8 +1,28 @@
+# while IFS='=' read -r line; do
+#   line=$(echo "$line" | tr -d '[:space:]') # Supprimer les espaces et tabulations
+#   if [[ ! "$line" =~ ^# ]] && [[ ! -z "$line" ]]; then # Ignorer les lignes vides et commençant par #
+#     key="${line%%=*}"
+#     value="${line#*=}"
+#     gh secret set "$key" <<< "$value"
+#   fi
+# done < .env
+
+#!/bin/bash
+
+# Nom de l'environnement
+ENVIRONMENT_NAME="production"
+
+# Créer l'environnement s'il n'existe pas déjà
+if ! gh environment list | grep -q "$ENVIRONMENT_NAME"; then
+  gh environment create "$ENVIRONMENT_NAME" --description "Environnement de production"
+fi
+
+# Charger les clés depuis le fichier .env
 while IFS='=' read -r line; do
   line=$(echo "$line" | tr -d '[:space:]') # Supprimer les espaces et tabulations
   if [[ ! "$line" =~ ^# ]] && [[ ! -z "$line" ]]; then # Ignorer les lignes vides et commençant par #
     key="${line%%=*}"
     value="${line#*=}"
-    gh secret set "$key" <<< "$value"
+    gh secret set "$key" --env "$ENVIRONMENT_NAME" <<< "$value"
   fi
 done < .env
